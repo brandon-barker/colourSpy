e876d68792509dfa10ae5a46c927327678f9af3f
Brandon Barker <skinty@gmail.com>
2014-08-17 17:32:22 +0200

    Added export functionality (export to CSS / SCSS / LESS / Plain Text or JSON)

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ colourSpy
 
 colourSpy is a handy Chrome extension to quickly find and preview colours used on a website.
 
-It displays them all in a list of swatches where you can easily copy the corresponding RGB, HEX or HSL values for use in your IDE of choice.
+It displays them all in a list of swatches where you can easily copy the corresponding RGB, HEX or HSL values for use in your IDE of choice, or simply export the list of colours to your format of choice.
+
+Supported export formats at the moment are:
+
+* CSS
+* SCSS
+* LESS
+* Plain Text
+* JSON
 
 ## Screenshots
 
@@ -13,12 +21,9 @@ It displays them all in a list of swatches where you can easily copy the corresp
 ![colourSpy](http://i.imgur.com/3E6Z47t.png)
 ![colourSpy](http://i.imgur.com/C9M7j8b.png)
 
-## Development
-
-Coming soon...
-
 ## Credit
 
 Made possible with the help of these awesome libraries
 
 [Color](https://github.com/harthur/color) by @harthur
+[FileSaver.js](https://github.com/eligrey/FileSaver.js) by @eligrey
--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -16,5 +16,30 @@ function MainCtrl($scope, $log) {
     });
   };
 
+  $scope.export = function (type) {
+    var filteredColours = _.pluck($scope.colours, $scope.mode);
+    var content = '';
+    var prefix = type === 'scss' ? '$' : type === 'less' ? '@' : '#';
+    var suffix = type === 'css' ? '.css' : type === 'scss' ? '.scss' : type === 'less' ? '.less' : type === 'json' ? '.json' : '.txt';
+    var mimeType = 'text/plain;charset=utf-8';
+
+    if (type === 'css' || type === 'scss' || type === 'less') {
+      angular.forEach(filteredColours, function (colour, index) {
+        var keyword = new Color(colour).keyword() || 'colour' + index;
+        content += prefix + keyword + ': ' + colour + '; \n';
+      });
+    }
+
+    if (type === 'json') {
+      mimeType = 'application/json;charset=utf-8';
+      content = JSON.stringify(filteredColours);
+    }
+
+    if (type === 'plain')
+      content = filteredColours.join('; \n');
+
+    saveAs(new Blob(_.toArray(content), {type: mimeType}), "colourSpy" + suffix);
+  };
+
   $scope.init();
 }
